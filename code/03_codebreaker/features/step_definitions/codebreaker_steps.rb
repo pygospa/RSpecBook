371e42e3fe5a3b81d47fd09e8e77b0d40d76a6f5
Kannan Thambiah <pygospa@gmail.com>
2014-05-26 10:17:17 +0200

    Add step definition templates for first feature

---
+++ code/03_codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -0,0 +1,15 @@
+Given /^I am not yet playing$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I start a new game$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I sould see "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
